52e86c960e5eefad69c63355d0a84e8cc398e7b8
Chiloong <76913920+Chiloong@users.noreply.github.com>
2026-04-06 19:52:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from wind import check_wind
+from pressure import check_pressure
 
 def main():
     check_wind()
+    check_pressure()
 
 if __name__ == "__main__":
     main()
-from wind import send_bark
-send_bark("✅系统运行成功")

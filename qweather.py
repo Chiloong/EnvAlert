f9b9d18287aef16e21862db6d310acc49f5aaf75
Chiloong <76913920+Chiloong@users.noreply.github.com>
2026-04-10 13:45:59 +0800

    Update qweather.py

--- qweather.py
+++ qweather.py
@@ -1,1 +1,19 @@
+import requests
+from config import *
 
+def get_all():
+    params = {
+        "location": f"{LON},{LAT}",
+        "key": QWEATHER_API_KEY
+    }
+
+    w = requests.get(QWEATHER_NOW, params=params, timeout=10).json()["now"]
+    a = requests.get(QWEATHER_AIR, params=params, timeout=10).json()["now"]
+
+    return {
+        "pressure": float(w["pressure"]),
+        "humidity": float(w["humidity"]),
+        "wind_speed": float(w["windSpeed"]),
+        "wind_dir": float(w["wind360"]),
+        "aqi": int(a["aqi"])
+    }

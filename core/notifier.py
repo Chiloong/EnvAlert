6d09472d320f358d1fc8d5fa13982c51a408b4ec
Chiloong <76913920+Chiloong@users.noreply.github.com>
2026-04-11 14:00:54 +0800

    Update notifier.py

--- core/notifier.py
+++ core/notifier.py
@@ -1,10 +1,16 @@
 import requests
 from config import BARK_KEY
+from urllib.parse import quote
 
 
 def send(msg):
     print("bark send")
-    url = f"https://api.day.app/{BARK_KEY}/{msg}"
+
+    # 🔥关键：URL编码
+    safe_msg = quote(msg)
+
+    url = f"https://api.day.app/{BARK_KEY}/{safe_msg}"
+
     try:
         r = requests.get(url, timeout=10)
         print(r.status_code)
